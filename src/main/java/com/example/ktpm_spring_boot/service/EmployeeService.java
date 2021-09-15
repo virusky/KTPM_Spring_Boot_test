61310e8df428826a93c1c45ca1a111fe7529e0bf
Dĩ Khang <ledikhang2609@gmail.com>
2021-09-15 23:33:48 +0700

    commit for service crud basic

--- src/main/java/com/example/ktpm_spring_boot/service/EmployeeService.java
+++ src/main/java/com/example/ktpm_spring_boot/service/EmployeeService.java
@@ -1,4 +1,15 @@
 package com.example.ktpm_spring_boot.service;
 
+import com.example.ktpm_spring_boot.entity.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface EmployeeService {
+    public Employee AddEmployee( Employee employee);
+    public List<Employee> GetAllEmployee();
+    public Employee GetEmployeeByID(Long id);
+    public Employee UpdateEmployeeByID(Long id, Employee employee);
+    public void DeteleEnployeeByID(Long id);
 }
--- src/main/java/com/example/ktpm_spring_boot/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/ktpm_spring_boot/service/EmployeeServiceImpl.java
@@ -1,4 +1,46 @@
 package com.example.ktpm_spring_boot.service;
 
-public class EmployeeServiceImpl {
+import com.example.ktpm_spring_boot.entity.Employee;
+import com.example.ktpm_spring_boot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public Employee AddEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public List<Employee> GetAllEmployee() {
+        List<Employee> employeeList = new ArrayList<>();
+        employeeRepository.findAll().forEach(employeeList::add);
+        return employeeList;
+    }
+
+    @Override
+    public Employee GetEmployeeByID(Long id) {
+        return employeeRepository.findById(id).get();
+    }
+
+    @Override
+    public Employee UpdateEmployeeByID(Long id, Employee employee) {
+        Employee emp = employeeRepository.findById(id).get();
+        emp.setName(employee.getName());
+        emp.setEmail(employee.getEmail());
+        return emp;
+    }
+
+    @Override
+    public void DeteleEnployeeByID(Long id) {
+        employeeRepository.deleteById(id);
+    }
 }
