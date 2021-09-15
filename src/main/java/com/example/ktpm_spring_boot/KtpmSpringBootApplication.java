2c470159037f77eb5f2f51e58ed6a0a25bf9158e
Dĩ Khang <ledikhang2609@gmail.com>
2021-09-15 22:46:11 +0700

    first commit

---
+++ src/main/java/com/example/ktpm_spring_boot/KtpmSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ktpm_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtpmSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtpmSpringBootApplication.class, args);
+    }
+
+}
